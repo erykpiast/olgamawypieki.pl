a81d1fc2c8678a548e2f12447d4a5704b89058d4
Eryk Napierała <eryk.piast@gmail.com>
2017-04-02 15:44:50 +0200

    add redirection to facebook

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.debug('3');
+import { parse as parseQuery } from 'query-string';
+
+const identity = (a) => a;
+
+const FACEBOOK_URL = 'https://www.facebook.com/olgamawypieki/';
+const queryParams = parseQuery(window.location.search);
+const redirectUrl = [
+  FACEBOOK_URL,
+  queryParams.ref && `ref=${queryParams.ref}`,
+].filter(identity).join('?');
+
+setTimeout(() => {
+  window.location = redirectUrl;
+}, 1000);
